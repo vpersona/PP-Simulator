b611b01c497aaf84a70cb92b5be35bb678cb607b
Admin <Admin@DESKTOP-UAOMUF8>
2024-12-13 19:19:19 +0100

    .

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -7,7 +7,7 @@ class Program
     {
         Console.WriteLine("Starting Simulator!\n");
         Lab3a();
-        
+        Lab3b();
     }
     
 
@@ -45,6 +45,25 @@ class Program
         Console.WriteLine(a.Info);
     }
 
+    static void Lab3b()
+    {
+        Creature c = new("Shrek", 7);
+        c.SayHi();
 
-  
+        Console.WriteLine("\n* Up");
+        c.Go(Direction.Up);
+
+        Console.WriteLine("\n* Right, Left, Left, Down");
+        Direction[] directions = {
+      Direction.Right, Direction.Left, Direction.Left, Direction.Down
+  };
+        c.Go(directions);
+
+        Console.WriteLine("\n* LRL");
+        c.Go("LRL");
+
+        Console.WriteLine("\n* xxxdR lyyLTyu");
+        c.Go("xxxdR lyyLTyu");
+    }
+
 }
